efed85f5e583267cae9ddb1a349e6e1c541b5ebf
Akira Ohgaki <akiraohgaki@gmail.com>
2017-08-25 11:36:59 +0900

    Add appimage.sh

---
+++ pkg/appimage/appimage.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PKGNAME='ocs-url'
+
+sh scripts/import.sh
+qmake PREFIX="/usr"
+make
+make INSTALL_ROOT="${PKGNAME}.AppDir" install
+
+curl -L -o linuxdeployqt.AppImage "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod 755 linuxdeployqt.AppImage
+./linuxdeployqt.AppImage --appimage-extract
+
+./squashfs-root/AppRun ${PKGNAME}.AppDir/usr/share/applications/${PKGNAME}.desktop -bundle-non-qt-libs -no-translations -qmldir="app/qml"
+./squashfs-root/AppRun ${PKGNAME}.AppDir/usr/share/applications/${PKGNAME}.desktop -appimage
