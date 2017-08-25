6dcf17dc376dbfbed584da46e572a030f2412e43
Akira Ohgaki <akiraohgaki@gmail.com>
2017-08-25 21:19:46 +0900

    Update appimage.sh

--- pkg/appimage/appimage.sh
+++ pkg/appimage/appimage.sh
@@ -7,14 +7,11 @@ qmake PREFIX="/usr"
 make
 make INSTALL_ROOT="${PKGNAME}.AppDir" install
 
-curl -L -o linuxdeployqt.AppImage "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-chmod 755 linuxdeployqt.AppImage
-./linuxdeployqt.AppImage --appimage-extract
+curl -L -o linuxdeployqt "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod 755 linuxdeployqt
+./linuxdeployqt --appimage-extract
 
 ./squashfs-root/AppRun ${PKGNAME}.AppDir/usr/share/applications/${PKGNAME}.desktop -bundle-non-qt-libs -no-translations -qmldir="app/qml"
 ./squashfs-root/AppRun ${PKGNAME}.AppDir/usr/share/applications/${PKGNAME}.desktop -appimage
-
-rm linuxdeployqt.AppImage
-rm -rf squashfs-root
 
 mv *.AppImage ${PKGNAME}-x86_64.AppImage
